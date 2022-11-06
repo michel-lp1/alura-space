95709f224c4cb10c1a88d2e8a7e00eb32850adef
michel-lp1 <michel.lp@outlook.com>
2022-11-06 10:58:57 -0300

    novos ajuses

---
+++ alura-space-projeto/src/componentes/Menu/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import home from '../../assets/icones/home.png'
+import maisCurtidas from '../../assets/icones/mais-curtidas.png'
+import maisVistas from '../../assets/icones/mais-vistas.png'
+import novas from '../../assets/icones/novas.png'
+import surpreendaMe from '../../assets/icones/surpreenda-me.png'
+import styles from './Menu.module.scss';
+
+export default function Menu() {
+  return (
+    <nav className={styles.menu}>
+      <ul className={styles.menu__lista}>
+        <li className={styles.menu__item}>
+          <img src={home} alt="" />
+          <a href="/">Início</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={maisCurtidas} alt="" />
+          <a href="/">Mais curtidas</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={maisVistas} alt="" />
+          <a href="/">Mais vistas</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={novas} alt="" />
+          <a href="/">Novas</a>
+        </li>
+        <li className={styles.menu__item}>
+          <img src={surpreendaMe} alt="" />
+          <a href="/">Surpreenda-me</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
--- alura-space-projeto/src/paginas/PaginaInicial/index.jsx
+++ alura-space-projeto/src/paginas/PaginaInicial/index.jsx
@@ -1,7 +1,21 @@
+import banner from "./banner.png";
 import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+import styles from "./PaginaInicial.module.scss";
 
-export default function PaginaInicial(){
+export default function PaginaInicial() {
   return (
-    <Cabecalho />
-  )
+    <>
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
